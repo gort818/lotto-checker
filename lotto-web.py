5ab665087450f1dd202bfede44ef2ea42ef181dc
Alessandro <gort818@gmail.com>
2018-12-26 19:54:21 -0800

    added while loop and powerball numbers

--- lotto-web.py
+++ lotto-web.py
@@ -19,65 +19,86 @@ def super_lotto():
 
 def mega_millions():
     lotto_list=[]
-    page=requests.get('http://www.megamillions.com/winning-numbers')
+
+    page=requests.get('https://www.calottery.com/play/draw-games/mega-millions')
     page.raise_for_status()
     soup=bs4.BeautifulSoup(page.text,"lxml")
-    for div in soup.find_all("div",class_="winning-numbers-white-ball"):
-        #print(div.text)
-        num=div.text
-        num=''.join(num.split())
-        lotto_list.append(num)   
-    for div in soup.find_all("div",class_="winning-numbers-mega-ball"):
-        num=div.text
-        num=''.join(num.split())
-        lotto_list.append(num) 
+
+    for ultag in soup.find_all('ul', {'class': 'winning_number_sm'}):
+        for litag in ultag.find_all('li'):
+            lotto_list.append(litag.text)
     lotto_list = [int(i) for i in lotto_list]
     return lotto_list
-    #print(lotto_list)
-    #print(type(lotto_list))
+
+
+def power_ball():
+    lotto_list=[]
+
+    page=requests.get('https://www.calottery.com/play/draw-games/powerball')
+    page.raise_for_status()
+    soup=bs4.BeautifulSoup(page.text,"lxml")
+
+    for ultag in soup.find_all('ul', {'class': 'winning_number_sm'}):
+        for litag in ultag.find_all('li'):
+            lotto_list.append(litag.text)
+    lotto_list = [int(i) for i in lotto_list]
+    print(lotto_list)
+    return lotto_list
+
+
+def menu():
+    print("\nWelcome to Lotto Checker!\n")
+    print("1: Check Super Lotto")
+    print("2: Check Mega Millions")
+    print("3: Check PowerBall")
+    print("4: Quit\n")
+    return input("Choose a Selection\n")
+
+
 def main():
 
     choice ="0"
-    
 
-    print("Welcome to Lotto Checker!")
-    print("1: Check Super Lotto")
-    print("2: Check Mega Millions")
-    #print("3: Exit")
-    choice=input("Choose a Selection")
-    if choice =="1":
-        lotto =super_lotto()
-    elif choice =="2":
-        lotto =mega_millions()
-        
-    counter =0
-    mega =0
-    
-    #print(lotto)
-    print ("The Lotto Numbers are " + str(lotto)[1:-1])
-    user = [0,0,0,0,0,0]
+    while choice is not "4":
+        choice =menu()
+        if choice =="1":
+            lotto =super_lotto()
+            if  not lotto:
+                print("Results are not available yet try again later")
+                continue
+        elif choice == "2":
+            lotto=mega_millions()
+            if  not lotto:
+                print("Results are not available yet try again later")
+                continue
+        elif choice == "3":
+            lotto=power_ball()
+            if  not lotto:
+                print("Results are not available yet try again later")
+                continue
+        elif choice == "4":
+            quit()
+        else:
+            print("You have entered an invalid selection try again\n")
+            choice=menu()
+        counter =0
+        mega =0
+
+        #print(lotto)
+        print ("\nThe Lotto Numbers are " + str(lotto)[1:-1] )
+        user = [0,0,0,0,0,0]
 
 
-    def  no_duplicates(user,num):
+        def  no_duplicates(user,num):
+            for x in range(0,6):
+
+                if(user[x] == num):
+
+                    return True
+
+
         for x in range(0,6):
-
-            if(user[x] == num):
-
-                return True
-
-
-    for x in range(0,6):
-        num_lotto="Lotto Number: "+str(x+1)+"\n"
-        while True:
-            try:
-                num =int(input(num_lotto))
-                break
-            except ValueError:
-                print("Invalid number try again\n")
-
-
-        while no_duplicates(user,num) is True:
-            print(" You have already entered that number! please try again" )
+            num_lotto="Lotto Number: "+str(x+1)+"\n"
             while True:
                 try:
                     num =int(input(num_lotto))
@@ -86,28 +107,38 @@ def main():
                     print("Invalid number try again\n")
 
 
-        user[x]=num
+            while no_duplicates(user,num) is True:
+                print(" You have already entered that number! please try again\n" )
+                while True:
+                    try:
+                        num =int(input(num_lotto))
+                        break
+                    except ValueError:
+                        print("Invalid number try again\n")
 
 
-    for x in range(0,5):
+            user[x]=num
 
-            for y in range(0,5):
-                if lotto[x] == user[y]:
-                    print(" You have a match! it was " + str(user[y]) )
-                    y+=1
-                    counter += 1
-    if lotto[-1] == user[-1]:
-        mega =1
 
-    
+        for x in range(0,5):
 
-    print("The lotto numbers are:")
-    print (lotto)
-    print("Your numbers are:")
-    print(user)
-    print("you had " + str(counter) + "  matches")
-    if mega ==1:
-        print("You had matched the mega!")
+                for y in range(0,5):
+                    if lotto[x] == user[y]:
+                        print(" You have a match! it was " + str(user[y]) + "\n")
+                        y+=1
+                        counter += 1
+        if lotto[-1] == user[-1]:
+            mega =1
+
+
+
+        print("The lotto numbers are:\n")
+        print (lotto)
+        print("Your numbers are:")
+        print(user)
+        print("you had " + str(counter) + "  matches\n")
+        if mega ==1:
+            print("You had matched the mega!\n")
 
 
 main()
